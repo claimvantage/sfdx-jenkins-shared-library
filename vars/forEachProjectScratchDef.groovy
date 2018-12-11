ca9b2637685a21845c21a892e4b07b10d1a3842f
Keith Clarke <keithclarke@claimvantage.com>
2018-12-11 22:08:48 +0000

    Update forEachProjectScratchDef.groovy

--- vars/forEachProjectScratchDef.groovy
+++ vars/forEachProjectScratchDef.groovy
@@ -4,7 +4,9 @@ def call(Map parameters = [:], body) {
     
     for (def file : findFiles(glob: 'config/project-scratch-def.*.json')) {
         echo "Found ${file.path}"
-        body()
+        withEnv(["ORG_TYPE = ${file.name}"]) {
+            body()
+        }
     }
     
     return this
