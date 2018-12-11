38ef5f9f66852d09937f5dcb0ca0504105b26420
Keith Clarke <keithclarke@claimvantage.com>
2018-12-11 17:29:46 +0000

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -2,7 +2,7 @@
 
 def call() {
 
-    echo "Push to ${SFDC_USERNAME}"
+    echo "Push for ${SFDC_USERNAME}"
 
     def rc = sh returnStatus: true, script: "sfdx force:source:push --targetusername ${SFDC_USERNAME}"
     if (rc != 0) { error 'Push failed' }
