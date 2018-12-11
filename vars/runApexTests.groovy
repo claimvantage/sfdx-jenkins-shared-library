bc2c5a4e358e162433f4c8cdb47fe019ecc51e8e
Keith Clarke <keithclarke@claimvantage.com>
2018-12-11 17:29:58 +0000

    Create runApexTests.groovy

---
+++ vars/runApexTests.groovy
@@ -0,0 +1,15 @@
+#!/usr/bin/env groovy
+
+def call() {
+
+    echo "Run Apex tests for ${SFDC_USERNAME}"
+
+    def BUILD_NUMBER = env.BUILD_NUMBER
+    def RUN_ARTIFACT_DIR = "tests/${BUILD_NUMBER}"
+    
+    sh "mkdir -p ${RUN_ARTIFACT_DIR}"
+    def rc = sh returnStatus: true, script: "sfdx force:apex:test:run --synchronous --testlevel RunLocalTests --outputdir ${RUN_ARTIFACT_DIR} --resultformat tap --targetusername ${SFDC_USERNAME} --wait 180"
+    if (rc != 0) { error 'Apex test run failed' }
+    
+    return this
+}
