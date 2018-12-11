cab0137613d72de021e82b8a4fb788a8757d1691
Keith Clarke <keithclarke@claimvantage.com>
2018-12-11 21:58:32 +0000

    Update forEachProjectScratchDef.groovy

--- vars/forEachProjectScratchDef.groovy
+++ vars/forEachProjectScratchDef.groovy
@@ -1,2 +1,11 @@
-for (def file : findFiles(glob: 'config/project-scratch-def.*.json')) {
+#!/usr/bin/env groovy
+
+def call(Map parameters = [:], body) {
+    
+    for (def file : findFiles(glob: 'config/project-scratch-def.*.json')) {
+        echo "Found ${file.path}"
+        body()
+    }
+    
+    return this
 }
