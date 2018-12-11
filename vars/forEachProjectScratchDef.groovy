4762f5776c620748954f7e87b78934484123b3c4
Keith Clarke <keithclarke@claimvantage.com>
2018-12-11 22:12:54 +0000

    Update forEachProjectScratchDef.groovy

--- vars/forEachProjectScratchDef.groovy
+++ vars/forEachProjectScratchDef.groovy
@@ -5,6 +5,7 @@ def call(Map parameters = [:], body) {
     for (def file : findFiles(glob: 'config/project-scratch-def.*.json')) {
         echo "Found ${file.path}"
         // TODO pull out part that the * matched
+        // TODO broken in parallel?
         def org = file.name
         withEnv(["ORG = ${org}"]) {
             body()
