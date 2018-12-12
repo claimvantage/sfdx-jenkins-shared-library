4a92920029bfac8575fd7751f47c34abd0c186d5
Keith Clarke <keithclarke@claimvantage.com>
2018-12-12 22:00:45 +0000

    Update runApexTests.groovy

--- vars/runApexTests.groovy
+++ vars/runApexTests.groovy
@@ -1,20 +1,18 @@
 #!/usr/bin/env groovy
+import com.claimvantage.jsl.Org
 
 def call(Org org) {
 
     echo "Run Apex tests for ${org}"
 
     def BUILD_NUMBER = env.BUILD_NUMBER
-    def TEST_RESULTS_DIR = "tests/${BUILD_NUMBER}"
+    def TEST_RESULTS_DIR = "tests/${BUILD_NUMBER}/${org.name}"
     
     sh "mkdir -p ${TEST_RESULTS_DIR}"
-    shWithStatus "sfdx force:apex:test:run --synchronous --testlevel RunLocalTests --outputdir ${RUN_ARTIFACT_DIR} --resultformat tap --targetusername ${org.username} --wait 180"
+    shWithStatus "sfdx force:apex:test:run --synchronous --testlevel RunLocalTests --outputdir ${TEST_RESULTS_DIR} --resultformat tap --targetusername ${org.username} --wait 180"
     
     // Prefix class name with target org to separate the test results
-    // TODO need separate folder perhaps
     shWithStatus "sed -i -- 's/classname=\"/classname=\"${org.name}./g' ${TEST_RESULTS_DIR}/*-junit.xml"
-    
-    junit keepLongStdio: true, testResults: 'tests/**/*-junit.xml'
     
     return this
 }
