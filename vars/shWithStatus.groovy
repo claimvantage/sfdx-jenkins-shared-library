f530555585b4c4ef30afb3910b11453eec0482ba
Keith Clarke <keithclarke@claimvantage.com>
2018-12-12 08:58:56 +0000

    Create shWithStatus.groovy

---
+++ vars/shWithStatus.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+
+def call(scriptText) {
+
+    def status = sh returnStatus: true, script: scriptText
+    if (status != 0) error "Script ${scriptText} failed: status ${status}"
+    
+    return this
+}
