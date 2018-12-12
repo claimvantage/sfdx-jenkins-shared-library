8b7e7d8cf56e929f0785442f5ba0dbd905abe54d
Keith Clarke <keithclarke@claimvantage.com>
2018-12-12 13:33:25 +0000

    Update Org.groovy

--- com/claimvantage/jsl/Org.groovy
+++ com/claimvantage/jsl/Org.groovy
@@ -5,7 +5,7 @@ class Org implements Serializable {
     // E.g. "config/project-scratch-def.encryption.json"
     String projectScratchDefPath;
   
-    // Result of scratch org creation e.g. "test-drgwjqh3xsn0@example.com" and "g54ncgf4!sdf<2"
+    // Filled in by scratch org creation e.g. "test-drgwjqh3xsn0@example.com" and "g54ncgf4!sdf<2"
     String username;
     String password;
     
@@ -16,7 +16,7 @@ class Org implements Serializable {
     
     // Extracted from projectScratchDefPath e.g. "encryption"
     String getName() {
-        def parts = projectScratchDefPath.split('\.')
-        return parts.length > 2 ? parts[parls.length - 2] : null
+        def parts = projectScratchDefPath.split('\\.')
+        return parts.length > 2 ? parts[parts.length - 2] : null
     }
 }
