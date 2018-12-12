9e82c36410e1b375cbc82703ea0170662d23094f
Keith Clarke <keithclarke@claimvantage.com>
2018-12-12 14:57:12 +0000

    Update runApexTests.groovy

--- vars/runApexTests.groovy
+++ vars/runApexTests.groovy
@@ -1,14 +1,14 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(Org org) {
 
-    echo "Run Apex tests for ${SFDC_USERNAME}"
+    echo "Run Apex tests for ${org}"
 
     def BUILD_NUMBER = env.BUILD_NUMBER
     def RUN_ARTIFACT_DIR = "tests/${BUILD_NUMBER}"
     
     sh "mkdir -p ${RUN_ARTIFACT_DIR}"
-    shWithStatus "sfdx force:apex:test:run --synchronous --testlevel RunLocalTests --outputdir ${RUN_ARTIFACT_DIR} --resultformat tap --targetusername ${SFDC_USERNAME} --wait 180"
+    shWithStatus "sfdx force:apex:test:run --synchronous --testlevel RunLocalTests --outputdir ${RUN_ARTIFACT_DIR} --resultformat tap --targetusername ${org.username} --wait 180"
     junit keepLongStdio: true, testResults: 'tests/**/*-junit.xml'
     
     return this
