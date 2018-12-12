df4d2833e0f3117dc2f52be836d2b38db7f85623
Keith Clarke <keithclarke@claimvantage.com>
2018-12-12 22:31:32 +0000

    Update retrieveExternals.groovy

--- vars/retrieveExternals.groovy
+++ vars/retrieveExternals.groovy
@@ -2,14 +2,12 @@ def call() {
 
     echo "Retrieve externals"
     
-    withCredentials([sshUserPrivateKey(credentialsId: JPK, keyFileVariable: 'jenkins_private_key')]) {
-        sh '''
-        which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )
-        eval $(ssh-agent -s)
-        ssh-add ${jenkins_private_key}
-        git externals update
-        '''
-    }
+    sh '''
+    which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )
+    eval $(ssh-agent -s)
+    ssh-add ${jenkins_private_key}
+    git externals update
+    '''
     
     return this
 }
