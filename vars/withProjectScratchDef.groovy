66abf456fae690208cefd602437d847875ef7ffa
Keith Clarke <keithclarke@claimvantage.com>
2018-12-12 13:35:15 +0000

    Update and rename forEachProjectScratchDef.groovy to withProjectScratchDef.groovy

--- vars/withProjectScratchDef.groovy
+++ vars/withProjectScratchDef.groovy
@@ -1,12 +1,12 @@
 #!/usr/bin/env groovy
 
+import com.claimvantage.jsl.Org
+
 def call(Map parameters = [:], body) {
     
     for (def file : findFiles(glob: 'config/project-scratch-def.*.json')) {
         echo "Found ${file.path}"
-        // TODO pull out part that the * matched
-        // Probably OK in closure if a def is used to capture the value
-        def org = file.name
+        def org = new Org(file.path)
         withEnv(["ORG = ${org}"]) {
             body()
         }
