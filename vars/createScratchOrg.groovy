26b329f7876e2d0813b302c224bcae9ee9af60eb
Keith Clarke <keithclarke@claimvantage.com>
2018-12-12 09:01:24 +0000

    Update createScratchOrg.groovy

--- vars/createScratchOrg.groovy
+++ vars/createScratchOrg.groovy
@@ -16,12 +16,9 @@ def call() {
     
     withCredentials([file(credentialsId: JWT_KEY_CRED_ID, variable: 'jwt_key_file')]) {
 
-        def rc = sh returnStatus: true, script: "sfdx force:auth:jwt:grant --clientid ${CONNECTED_APP_CONSUMER_KEY} --username ${HUB_ORG} --jwtkeyfile ${jwt_key_file} --setdefaultdevhubusername --instanceurl ${SFDC_HOST}"
-        if (rc != 0) { error 'Hub org authorization failed' }
+        shWithStatus "sfdx force:auth:jwt:grant --clientid ${CONNECTED_APP_CONSUMER_KEY} --username ${HUB_ORG} --jwtkeyfile ${jwt_key_file} --setdefaultdevhubusername --instanceurl ${SFDC_HOST}"
         
-        def json = sh returnStdout: true, script: "sfdx force:org:create --definitionfile config/project-scratch-def.json --json --setdefaultusername"
-        def obj = new groovy.json.JsonSlurperClassic().parseText(json)
-        if (obj.status != 0) { error 'Scratch org creation failed: ' + obj.message }
+        def obj = shWithResult "sfdx force:org:create --definitionfile config/project-scratch-def.json --json --setdefaultusername"
         SFDC_USERNAME = obj.result.username
         
         echo "Username for scratch org is ${SFDC_USERNAME}"
