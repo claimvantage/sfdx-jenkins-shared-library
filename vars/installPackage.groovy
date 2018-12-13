8b300f2229c52f53d396dac09fcf0bd807cc32c3
Keith Clarke <keithclarke@claimvantage.com>
2018-12-13 19:08:39 +0000

    Update installPackage.groovy

--- vars/installPackage.groovy
+++ vars/installPackage.groovy
@@ -10,12 +10,7 @@ def call(Map parameters = [:]) {
     
     echo "Install package ${name} in org ${org.name}"
     
-    def JWT_KEY_CRED_ID_DH = env.JWT_CRED_ID_DH
-
-    // Copy credentials file
-    withCredentials([file(credentialsId: JWT_KEY_CRED_ID_DH, variable: 'jwt_key_file')]) {
-        shWithStatus "sfdx force:package:install --targetusername ${org.username} --package ${versionId} --installationkey ${password} --wait 15 --noprompt"
-    }
+    shWithStatus "sfdx force:package:install --targetusername ${org.username} --package ${versionId} --installationkey ${password} --wait 15 --noprompt"
     
     echo "Installed package ${name} in org ${org.name}"
 }
