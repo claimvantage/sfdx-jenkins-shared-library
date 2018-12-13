17e78506f1ff612c4ed6ea90b32c24f748b7fdcd
Keith Clarke <keithclarke@claimvantage.com>
2018-12-13 16:24:42 +0000

    Update Org.groovy

--- src/com/claimvantage/jsl/Org.groovy
+++ src/com/claimvantage/jsl/Org.groovy
@@ -28,6 +28,7 @@ class Org implements Serializable {
     // Extracted from projectScratchDefPath e.g. "encryption"
     String getName() {
         def parts = projectScratchDefPath.split('\\.')
+        echo "Parts ${parts}"
         return parts.length > 2 ? parts[parts.length - 2] : '_default_'
     }
 }
