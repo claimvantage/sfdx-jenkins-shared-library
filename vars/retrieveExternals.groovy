d818c01e5a8ce3e104f8ea9d9897d3de2d225e3f
Keith Clarke <keithclarke@claimvantage.com>
2018-12-13 12:52:27 +0000

    Update retrieveExternals.groovy

--- vars/retrieveExternals.groovy
+++ vars/retrieveExternals.groovy
@@ -2,6 +2,7 @@ def call() {
 
     echo "Retrieve externals"
     
+    // The name of the credentials (added via the root "Credentials" link in Jenkins
     def JPK = env.JENKINS_PRIVATE_KEY_ID
     if (!JPK) error "env.JENKINS_PRIVATE_KEY_ID must be set"
     
