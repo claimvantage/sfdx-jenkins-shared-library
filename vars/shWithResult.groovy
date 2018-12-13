14d870f6b164d4526cd5867208c32991bb208f38
Keith Clarke <keithclarke@claimvantage.com>
2018-12-13 15:56:42 +0000

    Update shWithResult.groovy

--- vars/shWithResult.groovy
+++ vars/shWithResult.groovy
@@ -1,12 +1,16 @@
 #!/usr/bin/env groovy
 
-def call(scriptText) {
+def call(script) {
     
-    def json = sh returnStdout: true, script: scriptText
+    echo "Script ${script}"
+    
+    def json = sh returnStdout: true, script: script
     def object = new groovy.json.JsonSlurperClassic().parseText(json);
     if (object.status != 0) {
         error "Script ${scriptText} failed: status ${object.status} message: ${object.message} json: ${json}"
     }
+    
+    echo "Result ${object.result}"
     
     return object.result
 }
