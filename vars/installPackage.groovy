8e9618ed8382cdd2cf4df854b7d3356a299a0da2
Keith Clarke <keithclarke@claimvantage.com>
2018-12-13 18:17:26 +0000

    Update installPackage.groovy

--- vars/installPackage.groovy
+++ vars/installPackage.groovy
@@ -2,10 +2,12 @@ import com.claimvantage.jsl.Org
 
 // Name is just for info purposes
 def call(Org org, name, versionId, password) {
-
-    echo "Install package ${name} in org ${org.name}"
-
-    shWithStatus "sfdx force:package:install --targetusername ${org.username} --package ${versionId} --installationkey ${password} --wait 15 --noprompt"
     
-    echo "Installed package ${name} in org ${org.name}"
+    echo "Called............"
+
+    // echo "Install package ${name} in org ${org.name}"
+
+    // shWithStatus "sfdx force:package:install --targetusername ${org.username} --package ${versionId} --installationkey ${password} --wait 15 --noprompt"
+    
+    // echo "Installed package ${name} in org ${org.name}"
 }
