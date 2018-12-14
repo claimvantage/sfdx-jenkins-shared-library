eb9dfc80d121b3054ec671bb2f03aa43a685bef4
Keith Clarke <keithclarke@claimvantage.com>
2018-12-14 17:03:50 +0000

    Update runApexTests.groovy

--- vars/runApexTests.groovy
+++ vars/runApexTests.groovy
@@ -6,11 +6,13 @@ def call(Org org) {
     echo "Run Apex tests for ${org}"
 
     def BUILD_NUMBER = env.BUILD_NUMBER
-    def TEST_RESULTS_DIR = "tests/${BUILD_NUMBER}/${org.name}"
     
-    sh "mkdir -p ${TEST_RESULTS_DIR}"
-    shWithStatus "sfdx force:apex:test:run --synchronous --testlevel RunLocalTests --outputdir ${TEST_RESULTS_DIR} --resultformat tap --targetusername ${org.username} --wait 180"
+    // Separate tests by org name
+    def testResultsDir = "tests/${BUILD_NUMBER}/${org.name}"
+    
+    sh "mkdir -p ${testResultsDir}"
+    shWithStatus "sfdx force:apex:test:run --synchronous --testlevel RunLocalTests --outputdir ${testResultsDir} --resultformat tap --targetusername ${org.username} --wait 180"
     
     // Prefix class name with target org to separate the test results
-    shWithStatus "sed -i -- 's/classname=\"/classname=\"${org.name}./g' ${TEST_RESULTS_DIR}/*-junit.xml
+    shWithStatus "sed -i -- 's/classname=\"/classname=\"${org.name}./g' ${testResultsDir}/*-junit.xml"
 }
