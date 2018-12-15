31690fff0425bf9672173e61cf896cb6aac89802
Keith Clarke <keithclarke@claimvantage.com>
2018-12-15 10:48:54 +0000

    Update orgs.groovy

--- vars/orgs.groovy
+++ vars/orgs.groovy
@@ -1,11 +1,13 @@
 #!/usr/bin/env groovy
 import com.claimvantage.jsl.Org
+import java.util.ArrayList
+import java.util.ArrayList
 
 List<Org> call(String glob ) {
 
     List<Org> orgs = new ArrayList<Org>();
     
-    for (def scratchDefFile in findFiles(glob: 'config/project-scratch-def.*.json')) {
+    for (def scratchDefFile in findFiles(glob: glob)) {
         Org org = new Org(scratchDefFile.path)
         orgs.add(org)
     }
