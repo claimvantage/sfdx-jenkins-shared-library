a5f9e8e70e7418819ffe6dd37a014be709a28c6d
Keith Clarke <keithclarke@claimvantage.com>
2018-12-15 15:31:03 +0000

    Update retrieveExternals.groovy

--- vars/retrieveExternals.groovy
+++ vars/retrieveExternals.groovy
@@ -6,11 +6,13 @@ def call() {
     def JPK = env.JENKINS_PRIVATE_KEY_ID
     if (!JPK) error "env.JENKINS_PRIVATE_KEY_ID must be set"
     
-    // Want to throw away the noisy output; TODO ${jenkins_private_key} here?
-    sh returnStdout:true, script: '''
-    which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )
-    eval $(ssh-agent -s)
-    ssh-add ${jenkins_private_key}
-    git externals update
-    '''
+    withCredentials([sshUserPrivateKey(credentialsId: JPK, keyFileVariable: 'jenkins_private_key')]) {
+        // Want to throw away the noisy output; TODO ${jenkins_private_key} here?
+        sh returnStdout:true, script: '''
+        which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )
+        eval $(ssh-agent -s)
+        ssh-add ${jenkins_private_key}
+        git externals update
+        '''
+    }
 }
