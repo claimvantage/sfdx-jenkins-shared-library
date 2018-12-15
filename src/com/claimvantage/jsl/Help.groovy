1f15e33f1336f532fff71b187c35e5716c20f056
Keith Clarke <keithclarke@claimvantage.com>
2018-12-15 20:30:46 +0000

    Update Help.groovy

--- src/com/claimvantage/jsl/Help.groovy
+++ src/com/claimvantage/jsl/Help.groovy
@@ -6,6 +6,10 @@ class Help implements Serializable {
     String repository
     String rootPageId
     String spaceKey
+  
+    // For named args case
+    Help() {
+    }
     
     Help(String repository, String rootPageId, String spaceKey) {
         this.repository = repository
