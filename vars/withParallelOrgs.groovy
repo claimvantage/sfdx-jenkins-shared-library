0b0f76b80041d2c2f52410f6e45f76fb63d98c2e
Keith Clarke <keithclarke@claimvantage.com>
2018-12-15 11:47:39 +0000

    Update withParallelOrgs.groovy

--- vars/withParallelOrgs.groovy
+++ vars/withParallelOrgs.groovy
@@ -6,7 +6,7 @@ def call(Closure body = null) {
     def workspaceRoot = "${env.WORKSPACE}"
     def perOrgStages = [:]
     for (def scratchDefFile in findFiles(glob: 'config/project-scratch-def.*.json')) {
-        Org org = new Org(scratchDefFile.path)
+        Org org = new Org("${workspaceRoot}/${scratchDefFile.path}")
         perOrgStages["${org.name}"] = {
             ws(dir: "${workspaceRoot}/${org.name}") {
                 withCredentials([file(credentialsId: env.JWT_CRED_ID_DH, variable: 'jwt_key_file')]) {
