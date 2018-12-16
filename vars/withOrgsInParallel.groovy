e2e404d60d940f3d4a6b34798409cc9282a19f33
Keith Clarke <keithclarke@claimvantage.com>
2018-12-16 10:59:33 +0000

    Update withOrgsInParallel.groovy

--- vars/withOrgsInParallel.groovy
+++ vars/withOrgsInParallel.groovy
@@ -10,9 +10,7 @@ def call(Closure body = null) {
         def perOrgStages = [:]
         for (def scratchDefFile in findFiles(glob: 'config/project-scratch-def.*.json')) {
             Org org = new Org("${workspaceRoot}/${scratchDefFile.path}")
-            perOrgStages["${org.name}"] = {
-                body(org)
-            }
+            perOrgStages["${org.name}"] = body(org)
         }
         parallel perOrgStages
     }
