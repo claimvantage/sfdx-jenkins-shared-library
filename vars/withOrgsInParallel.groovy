cc70ecfd3261058d928ade268e6f0d16aa7efd40
Keith Clarke <keithclarke@claimvantage.com>
2018-12-16 11:14:10 +0000

    Update withOrgsInParallel.groovy

--- vars/withOrgsInParallel.groovy
+++ vars/withOrgsInParallel.groovy
@@ -10,6 +10,7 @@ def call(Map parameters = [:], Closure body = null) {
     withCredentials([file(credentialsId: env.JWT_CRED_ID_DH, variable: 'jwt_key_file')]) {
         def perOrgStages = [:]
         for (def scratchDefFile in findFiles(glob: glob)) {
+            echo "Found scratch def file ${scratchDefFile.path}"
             Org org = new Org("${scratchDefFile.path}")
             perOrgStages["${org.name}"] = {
                 body(org)
