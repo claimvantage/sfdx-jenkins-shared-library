1d6a248280987940799799427e45fbd2246c91d7
Keith Clarke <keithclarke@claimvantage.com>
2018-12-16 11:04:10 +0000

    Update withOrgsInParallel.groovy

--- vars/withOrgsInParallel.groovy
+++ vars/withOrgsInParallel.groovy
@@ -1,11 +1,15 @@
 #!/usr/bin/env groovy
 import com.claimvantage.jsl.Org
 
-def call(Closure body = null) {
+def call(Map parameters = [:], Closure body = null) {
+    
+    def glob = parameters.glob
+    if (!glob) glob = 'config/project-scratch-def.*.json'
+    echo "Finding scratch def files using expression ${glob}"
     
     withCredentials([file(credentialsId: env.JWT_CRED_ID_DH, variable: 'jwt_key_file')]) {
         def perOrgStages = [:]
-        for (def scratchDefFile in findFiles(glob: 'config/project-scratch-def.*.json')) {
+        for (def scratchDefFile in findFiles(glob: glob)) {
             Org org = new Org("${scratchDefFile.path}")
             perOrgStages["${org.name}"] = {
                 body(org)
@@ -13,55 +17,4 @@ def call(Closure body = null) {
         }
         parallel perOrgStages
     }
-    
-    /*
-    withCredentials([file(credentialsId: env.JWT_CRED_ID_DH, variable: 'jwt_key_file')]) {
-        def perOrgStages = [:]
-        for (def scratchDefFile in findFiles(glob: 'config/project-scratch-def.*.json')) {
-            Org org = new Org("${scratchDefFile.path}")
-            perOrgStages["${org.name}"] = {
-                if (body) {
-                    body(org)
-                }
-            }
-        }
-        parallel perOrgStages
-    }
-    */
-    
-    /*
-    def workspaceRoot = "${env.WORKSPACE}"
-    env['WORKSPACE_ROOT'] = workspaceRoot
-    
-    def perOrgStages = [:]
-    for (def scratchDefFile in findFiles(glob: 'config/project-scratch-def.*.json')) {
-        node {
-            Org org = new Org("${workspaceRoot}/${scratchDefFile.path}")
-            perOrgStages["${org.name}"] = {
-                ws(dir: "${workspaceRoot}/../withParallelOrgs-${org.name}") {
-                    withCredentials([file(credentialsId: env.JWT_CRED_ID_DH, variable: 'jwt_key_file')]) {
-                        if (body) {
-                            echo "+++ Calling body ${org.name}"
-                            body(org)
-                            echo "+++ Returned from body ${org.name}"
-                        }
-                    }
-                }
-            }
-        }
-    }
-    parallel perOrgStages
-    */
-    
-    /*
-    for (def file : findFiles(glob: glob)) {
-        echo "Found ${file.path}"
-        def org = new Org(projectScratchDefPath: file.path)
-        if (body) {
-            body([variable: org])
-        }
-    }
-    
-    return this
-    */
 }
