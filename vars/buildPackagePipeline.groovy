2d34f009446e402808d86ea182a755f6f7549b6e
Keith Clarke <keithclarke@claimvantage.com>
2018-12-16 16:08:05 +0000

    Update buildPackagePipeline.groovy

--- vars/buildPackagePipeline.groovy
+++ vars/buildPackagePipeline.groovy
@@ -4,13 +4,49 @@ import com.claimvantage.jsl.Package
 
 def call(Map parameters = [:]) {
     
-    Help h
-    if (parameters.help) {
-        h = (Help) parameters.help;
-    }
+    String glob = parameters.glob
+    Help help = (Help) parameters.help
+    Package[] packages = (Package[]) parameters.packages;
     
-    Package[] ps
-    if (parameters.packages) {
-        ps = (Package[]) parameters.packages;
+    pipeline {
+        stages {
+            if (help) {
+                stage("help") {
+                    processHelp(help: new Help('cx', '33226968', 'extras-help'))
+                }
+            }
+            stage("checkout") {
+                checkout(scm: scm, quiet: true)
+                retrieveExternals()
+            }
+            withOrgsInParallel(glob: glob) { org ->
+                stage("${org.name} create") {
+                    createScratchOrg org
+                }
+                if (packages && packages.size() > 0) {
+                    stage("${org.name} install") {
+                        for (Package p in packages) {
+                            installPackage(org: org, p)
+                        }
+                    }
+                }
+                stage("${org.name} push") {
+                    pushToOrg org
+                }
+                stage("${org.name} test") {
+                    runApexTests org
+                }
+                stage("${org.name} delete") {
+                    deleteScratchOrg org
+                }
+            }
+            stage('publish') {
+                publishTestResults()
+            }
+            stage('clean') {
+                // Always remove workspace and don't fail the build for any errors
+                cleanWs notFailBuild: true
+            }
+        }
     }
 }
