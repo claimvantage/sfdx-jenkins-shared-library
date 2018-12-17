5dd23db454fa890daba6609d1891268aedff8922
Keith Clarke <keithclarke@claimvantage.com>
2018-12-17 17:29:52 +0000

    Update retrieveExternals.groovy

--- vars/retrieveExternals.groovy
+++ vars/retrieveExternals.groovy
@@ -6,11 +6,7 @@ def call() {
     
     // TODO no-op if no get_externals.json
     
-    // The name of the credentials (added via the root "Credentials" link in Jenkins
-    def JPK = env.JENKINS_PRIVATE_KEY_ID
-    if (!JPK) error "env.JENKINS_PRIVATE_KEY_ID must be set"
-    
-    withCredentials([sshUserPrivateKey(credentialsId: JPK, keyFileVariable: 'jenkins_private_key')]) {
+    withCredentials([sshUserPrivateKey(credentialsId: env.GITHUB_CREDENTIAL_ID, keyFileVariable: 'jenkins_private_key')]) {
         // Want to throw away the noisy output; TODO ${jenkins_private_key} here?
         sh returnStdout:true, script: '''
         which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )
