6eda9b524f160294367f5a1f3799922abc4d3f9f
Pablo Fischer <pablofischer@claimvantage.com>
2018-12-18 14:50:48 +0000

    lightningTestRun added

---
+++ vars/lightningTestRun.groovy
@@ -0,0 +1,18 @@
+#!/usr/bin/env groovy
+import com.claimvantage.sjsl.Org
+
+def call(Map parameters = [:]) {
+
+    Org org = (Org) parameters.org
+    String configFile = parameters.configFile
+
+    // Separate tests by build number and org name
+    def testResultsDir = "${env.WORKSPACE}/lightning-tests/${env.BUILD_NUMBER}/${org.name}"
+
+    shWithStatus "sfdx force:lightning:test:run --configfile ${configFile} --targetusername ${org.username} --appname ${env.BUILD_NUMBER} --outputdir ${testResultsDir}"
+    
+    echo "Evaluate test results..."
+    def json = readFile(file:"${testResultsDir}/lightning-test-result.json")
+    def obj = new JsonSlurperClassic().parseText(json)
+    if (obj.summary.outcome != "Passed") { error 'Lightning validation failed. Pass Rate: ' + obj.summary.passRate }
+}
