188c989a3a564c7df0010f1f6f7613c10d3ed35d
Pablo Fischer <pablofischer@claimvantage.com>
2018-12-18 16:24:20 +0000

    Creation folder order fix

--- vars/runLightningTests.groovy
+++ vars/runLightningTests.groovy
@@ -6,13 +6,13 @@ def call(Map parameters = [:]) {
     Org org = (Org) parameters.org
     String configFile = parameters.configFile
 
+    // Separate tests by build number and org name
+    def testResultsDir = "${env.WORKSPACE}/lightning-tests/${env.BUILD_NUMBER}/${org.name}"
+
     sh "mkdir -p ${testResultsDir}"
     echo "Created lightning test result dir ${testResultsDir}"
 
     echo "Running Apex tests for ${org.name} outputting to ${testResultsDir}"
-
-    // Separate tests by build number and org name
-    def testResultsDir = "${env.WORKSPACE}/lightning-tests/${env.BUILD_NUMBER}/${org.name}"
 
     shWithStatus "sfdx force:lightning:test:run --configfile ${configFile} --targetusername ${org.username} --appname ${env.BUILD_NUMBER} --outputdir ${testResultsDir}"
     
