8bbd1ea89af2f246cdce913c41a6f35788e61686
Keith Clarke <keithclarke@claimvantage.com>
2018-12-18 19:26:59 +0000

    Update pushToOrg.groovy

--- vars/pushToOrg.groovy
+++ vars/pushToOrg.groovy
@@ -3,7 +3,7 @@ import com.claimvantage.sjsl.Org
 
 def call(Org org) {
 
-    echo "Push for ${org}"
+    echo "Push for ${org.name}"
 
     // Trying to cut log noise
     // shWithStatus "sfdx force:source:push --targetusername ${org.username}"
