3fbe9395be58c013e7c257a3eab19b6675264f56
Pablo Fischer <pablofischer@claimvantage.com>
2018-12-19 13:44:43 +0000

    sfdx force:lightning:test:run sh

--- vars/runLightningTests.groovy
+++ vars/runLightningTests.groovy
@@ -8,15 +8,16 @@ def call(Map parameters = [:]) {
 
     // Separate tests by build number and org name
     def testResultsDir = "${env.WORKSPACE}/lightning-tests/${env.BUILD_NUMBER}/${org.name}"
+
     sh "mkdir -p ${testResultsDir}"
     echo "Created lightning test result dir ${testResultsDir}"
 
     echo "Running Apex tests for ${org.name} outputting to ${testResultsDir}"
 
-    def script = "sfdx force:lightning:test:run --configfile ${configFile} --targetusername ${org.username} --appname ${org.name}_${env.BUILD_NUMBER} --outputdir ${testResultsDir}"
-    def json = sh returnStdout: true, script: script
-    def object = new groovy.json.JsonSlurperClassic().parseText(json);
+    sh "sfdx force:lightning:test:run --configfile ${configFile} --targetusername ${org.username} --appname ${org.name}_${env.BUILD_NUMBER} --outputdir ${testResultsDir}"
     
     echo "Evaluate test results..."
-    if (object.summary.outcome != "Passed") { error 'Lightning validation failed. Pass Rate: ' + object.summary.passRate }
+    def json = readFile(file:"${testResultsDir}/lightning-test-result.json")
+    def obj = new groovy.json.JsonSlurperClassic().parseText(json)
+    if (obj.summary.outcome != "Passed") { error 'Lightning validation failed. Pass Rate: ' + obj.summary.passRate }
 }
