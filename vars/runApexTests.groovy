f86f790c1d5df38d21695e21b580c55890211535
Pablo Fischer <pablofischer@claimvantage.com>
2018-12-20 19:02:46 +0000

    runApexTests typo fix

--- vars/runApexTests.groovy
+++ vars/runApexTests.groovy
@@ -3,7 +3,7 @@ import com.claimvantage.sjsl.Org
 
 def call(Org org) {
 
-    // Separate tests by buuld number and org name
+    // Separate tests by build number and org name
     def testResultsDir = "${env.WORKSPACE}/tests/${env.BUILD_NUMBER}/${org.name}"
     
     sh "mkdir -p ${testResultsDir}"
@@ -12,7 +12,7 @@ def call(Org org) {
     echo "Running Apex tests for ${org.name} outputting to ${testResultsDir}"
     
     // Deliberately no status check
-    sh  returnStatus: true, script: "sfdx force:apex:test:run --synchronous --testlevel RunLocalTests --outputdir ${testResultsDir} --resultformat tap --targetusername ${org.username} --wait 180"
+    sh returnStatus: true, script: "sfdx force:apex:test:run --synchronous --testlevel RunLocalTests --outputdir ${testResultsDir} --resultformat tap --targetusername ${org.username} --wait 180"
     
     // Prefix class name with target org to separate the test results
     sh "sed -i -- 's/classname=\"/classname=\"${org.name}./g' ${testResultsDir}/*-junit.xml"
