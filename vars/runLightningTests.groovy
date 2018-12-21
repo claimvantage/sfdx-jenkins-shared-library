ccb176017b4282bf7b0936b564b11bf8c36a5825
Pablo Fischer <pablofischer@claimvantage.com>
2018-12-21 18:20:16 +0000

    runLightningTest - Config file optional
    Parameter name fixed + interpolation

--- vars/runLightningTests.groovy
+++ vars/runLightningTests.groovy
@@ -15,11 +15,7 @@ def call(Map parameters = [:]) {
 
     echo "Running lightning tests for ${org.name} outputting to ${testResultsDir}"
 
-<<<<<<< HEAD
     sh returnStatus: true, script: "sfdx force:lightning:test:run ${configFile} --targetusername ${org.username} --resultformat tap --appname ${appName} --outputdir ${testResultsDir}"
-=======
-    sh returnStatus: true, script: "sfdx force:lightning:test:run --configfile ${configFile} --targetusername ${org.username} --resultformat tap --appname ${appName} --outputdir ${testResultsDir}"
->>>>>>> b884604... runLightningTests added
     
     // Prefix class name with target org and app name to separate the test results
     sh "sed -i -- 's/classname=\"/classname=\"${org.name}.${appName}/g' ${testResultsDir}/*-junit.xml"
