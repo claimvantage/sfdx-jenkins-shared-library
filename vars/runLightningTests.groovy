e26576a345f5517964224b2650b0faf99494e52a
Pablo Fischer <pablofischer@claimvantage.com>
2018-12-24 15:42:46 +0000

    runLightningTests configfile parameter as optional

--- vars/runLightningTests.groovy
+++ vars/runLightningTests.groovy
@@ -5,7 +5,7 @@ def call(Map parameters = [:]) {
 
     Org org = (Org) parameters.org
     String appName = parameters.appName
-    String configFile = parameters.configFile
+    String configFile = parameters.configFile ? "--configfile ${parameters.configFile}" : ''
 
     // Separate tests by build number and org name
     def testResultsDir = "${env.WORKSPACE}/tests/lightning/${env.BUILD_NUMBER}/${org.name}"
@@ -15,7 +15,7 @@ def call(Map parameters = [:]) {
 
     echo "Running lightning tests for ${org.name} outputting to ${testResultsDir}"
 
-    sh returnStatus: true, script: "sfdx force:lightning:test:run --configfile ${configFile} --targetusername ${org.username} --resultformat tap --appname ${appName} --outputdir ${testResultsDir}"
+    sh returnStatus: true, script: "sfdx force:lightning:test:run ${configFile} --targetusername ${org.username} --resultformat tap --appname ${appName} --outputdir ${testResultsDir}"
     
     // Prefix class name with target org and app name to separate the test results
     sh "sed -i -- 's/classname=\"/classname=\"${org.name}.${appName}/g' ${testResultsDir}/*-junit.xml"
