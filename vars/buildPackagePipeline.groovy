84f848d0d8cd97d14d6d2f3596d0aaf396a645b2
Jéferson Chaves <1755734+jefersonchaves@users.noreply.github.com>
2018-12-24 10:50:54 +0000

    Update buildPackagePipeline.groovy
    Fix string interpolation issue.

--- vars/buildPackagePipeline.groovy
+++ vars/buildPackagePipeline.groovy
@@ -38,7 +38,7 @@ def call(Map parameters = [:]) {
                             logRotator(
                                 artifactDaysToKeepStr: '',
                                 artifactNumToKeepStr: '',
-                                daysToKeepStr: '${daysToKeepBranch',
+                                daysToKeepStr: '${daysToKeepBranch}',
                                 numToKeepStr: ''
                             )
                         ),
