db9b9329b2ffff2399f409bbfd71aadef0a546c2
Jéferson Chaves <1755734+jefersonchaves@users.noreply.github.com>
2019-01-23 00:07:09 +0000

    Create BaseTest.groovy

---
+++ test/BaseTest.groovy
@@ -0,0 +1,30 @@
+import org.junit.*
+import com.lesfurets.jenkins.unit.*
+import static groovy.test.GroovyAssert.*
+
+class BaseTest extends BasePipelineTest {
+    @Before
+    void setUp() {
+        super.setUp()
+
+        // load all steps from vars directory
+        new File("vars").eachFile { file ->
+            def name = file.name.replace(".groovy", "")
+
+            // register step with no args, example: retrieveExternals()
+            helper.registerAllowedMethod(name, []) { ->
+                loadScript(file.path)()
+            }
+
+            // register step with Map arg, example: sfdxBuildPipeline(glob: "a")
+            helper.registerAllowedMethod(name, [ Map ]) { opts ->
+                loadScript(file.path)(opts)
+            }
+            
+            // register step with Org arg, example: createScratchOrg(org)
+            helper.registerAllowedMethod(name, [ com.claimvantage.sjsl.Org ]) { opts ->
+                loadScript(file.path)(opts)
+            }
+        }
+    }
+}
