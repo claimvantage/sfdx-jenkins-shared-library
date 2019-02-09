02c3a51e6bb17e55df3c3c5d1385cb388b41604a
Keith Clarke <keithclarke@claimvantage.com>
2019-02-09 14:18:57 +0000

    Update runApexTests.groovy
    CPM-896

--- vars/runApexTests.groovy
+++ vars/runApexTests.groovy
@@ -12,7 +12,31 @@ def call(Org org) {
     echo "Running Apex tests for ${org.name} outputting to ${testResultsDir}"
     
     // Deliberately no status check
-    sh returnStatus: true, script: "sfdx force:apex:test:run --synchronous --testlevel RunLocalTests --outputdir ${testResultsDir} --resultformat tap --targetusername ${org.username} --wait 180"
+    // sh returnStatus: true, script: "sfdx force:apex:test:run --synchronous --testlevel RunLocalTests --outputdir ${testResultsDir} --resultformat tap --targetusername ${org.username} --wait 180"
+
+
+
+
+    // Try polling to avoid EAI_AGAIN errors
+    def r1 = shWithResult "sfdx force:apex:test:run  --synchronous --testlevel RunLocalTests --json"
+    def testRunId = r1.testRunId
+
+    def totalMinutes = 0
+    def status = ''
+    while (status != 'Completed') {
+        sleep 60
+        totalMinutes++;
+
+        def query = "select Status from ApexTestRunResult where AsyncApexJobId = '${testRunId}'"
+        def r2 = shWithResult "sfdx force:data:soql:query --usetoolingapi --query '${query}' --targetusername ${org.username} --json"
+        status = r2.records[0].Status;
+
+        echo "Test status ${status} after ${totalMinutes} minutes"
+    }
+    shWithStatus "sfdx force:apex:test:report -testrunid ${testRunId} --outputdir ${testResultsDir} --resultformat tap --targetusername ${org.username}"
+
+
+
     
     // Prefix class name with target org to separate the test results
     sh "sed -i -- 's/classname=\"/classname=\"${org.name}./g' ${testResultsDir}/*-junit.xml"
