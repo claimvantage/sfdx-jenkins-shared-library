ed41f8e17a122d353d3ce5b1e155d85b5d8883d7
Keith Clarke <keithclarke@claimvantage.com>
2019-02-10 11:36:15 +0000

    Update runApexTests.groovy

--- vars/runApexTests.groovy
+++ vars/runApexTests.groovy
@@ -11,36 +11,43 @@ def call(Org org) {
 
     echo "Running Apex tests for ${org.name} outputting to ${testResultsDir}"
     
-    // Deliberately no status check
-    // sh returnStatus: true, script: "sfdx force:apex:test:run --synchronous --testlevel RunLocalTests --outputdir ${testResultsDir} --resultformat tap --targetusername ${org.username} --wait 180"
+    def experiencingEaiAgainErrors = true
+    
+    if (experiencingEaiAgainErrors) {
+        
+        // Use polling to workaround EAI_AGAIN errors
+        
+        def r1 = shWithResult "sfdx force:apex:test:run --testlevel RunLocalTests --targetusername ${org.username} --json"
+        def testRunId = r1.testRunId
 
+        def totalSleeps = 0
+        def status = ''
+        while (status != 'Completed' && totalSleeps < 180) {
+        
+            sleep 60
+            totalSleeps++;
 
+            def query = "select Status, MethodsEnqueued, MethodsCompleted, MethodsFailed from ApexTestRunResult where AsyncApexJobId = '${testRunId}'"
+            def r2 = shWithResult "sfdx force:data:soql:query --usetoolingapi --query \"${query}\" --targetusername ${org.username} --json"
+            def record = r2.records[0]
+            
+            status = record.Status;
+            def enqueued = record.MethodsEnqueued
+            def completed = record.MethodsCompleted
+            def failed = record.MethodsFailed
 
-
-    // Try polling to avoid EAI_AGAIN errors
-    def r1 = shWithResult "sfdx force:apex:test:run --testlevel RunLocalTests --targetusername ${org.username} --json"
-    def testRunId = r1.testRunId
-
-    def totalMinutes = 0
-    def status = ''
-    while (status != 'Completed') {
-        sleep 60
-        totalMinutes++;
-
-        def query = "select Status, MethodsEnqueued, MethodsCompleted, MethodsFailed from ApexTestRunResult where AsyncApexJobId = '${testRunId}'"
-        def r2 = shWithResult "sfdx force:data:soql:query --usetoolingapi --query \"${query}\" --targetusername ${org.username} --json"
-        def record = r2.records[0]
-        status = record.Status;
-        def enqueued = record.MethodsEnqueued
-        def completed = record.MethodsCompleted
-        def failed = record.MethodsFailed
-
-        echo "Test run status is \"${status}\" enqueued ${enqueued} completed ${completed} failed ${failed} after ${totalMinutes} minutes"
+            echo "Test run status is \"${status}\" with ${completed} of ${enqueued} methods run (${failed} methods failed) after ${totalSleeps} 1 minute sleeps"
+        }
+    
+        // Deliberately no status check so build doesn't fail immediately
+        sh returnStatus: true, script: "sfdx force:apex:test:report --testrunid ${testRunId} --outputdir ${testResultsDir} --resultformat tap --targetusername ${org.username}"
+    } else {
+        
+        // Desired, simple approach
+        
+        // Deliberately no status check so build doesn't fail immediately
+        sh returnStatus: true, script: "sfdx force:apex:test:run --synchronous --testlevel RunLocalTests --outputdir ${testResultsDir} --resultformat tap --targetusername ${org.username} --wait 180"
     }
-    shWithStatus "sfdx force:apex:test:report --testrunid ${testRunId} --outputdir ${testResultsDir} --resultformat tap --targetusername ${org.username}"
-
-
-
     
     // Prefix class name with target org to separate the test results
     sh "sed -i -- 's/classname=\"/classname=\"${org.name}./g' ${testResultsDir}/*-junit.xml"
