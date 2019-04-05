8042fe39e195dd344931a4c79cc8cc7aa3b339c3
Jéferson Chaves <1755734+jefersonchaves@users.noreply.github.com>
2019-04-05 10:22:48 +0100

    Update shWithResult.groovy
    #45: replace scriptText -> script.

--- vars/shWithResult.groovy
+++ vars/shWithResult.groovy
@@ -7,7 +7,7 @@ def call(script) {
     def json = sh returnStdout: true, script: script
     def object = readJSON text: json
     if (object.status != 0) {
-        error "Script ${scriptText} failed: status ${object.status} message: ${object.message} json: ${json}"
+        error "Script ${script} failed: status ${object.status} message: ${object.message} json: ${json}"
     }
     
     return object.result
