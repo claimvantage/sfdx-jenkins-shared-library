d9da5ad9e8a405750c50318a5db890b6715ffd13
Francesco Pitzalis <francescopitzalis@claimvantage.com>
2019-07-10 16:45:55 +0100

    CPM-1030 Extended Help functionality

--- src/com/claimvantage/sjsl/Help.groovy
+++ src/com/claimvantage/sjsl/Help.groovy
@@ -6,14 +6,23 @@ class Help implements Serializable {
     String spaceKey
     String rootPageId
     String repository
-  
+    Boolean forceDownloadHelpFixer
+    String[] helpFixerParams
+
+
     // For named args case
     Help() {
     }
+
+    Help(String spaceKey, String rootPageId, String repository, String... helpFixerParams) {
+        this(spaceKey, rootPageId, repository, false, helpFixerParams)
+    }
     
-    Help(String spaceKey, String rootPageId, String repository) {
+    Help(String spaceKey, String rootPageId, String repository, Boolean forceDownloadHelpFixer, String... helpFixerParams) {
         this.spaceKey = spaceKey
         this.rootPageId = rootPageId
         this.repository = repository
+        this.forceDownloadHelpFixer = forceDownloadHelpFixer
+        this.helpFixerParams = helpFixerParams
     }
 }
