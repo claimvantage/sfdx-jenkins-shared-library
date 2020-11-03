9f8019092763359d812a2db9c1a37b0f9be7dffe
Jeferson Chaves <1755734+jefersonchaves@users.noreply.github.com>
2020-11-03 13:48:06 +0000

    #96: add function to find the latest version

--- vars/sfdxDeployPipeline.groovy
+++ vars/sfdxDeployPipeline.groovy
@@ -76,7 +76,8 @@ def shouldInstallPackage(Map parameters = [:]) {
     def installedPackages = parameters.installedPackages
     def installedVersion = installedPackages[packageNamespace]
 
-    def result = versionPossibleToInstall > installedVersion
+    // We don't want to keep re-installing
+    def result = installedVersion == versionPossibleToInstall ? false : mostRecentVersion([installedVersion, versionPossibleToInstall]) == versionPossibleToInstall
     echo """
         Name: ${packageName}, Namespace: ${packageNamespace}
         Installed Version ${installedVersion} > Version to Install ${versionPossibleToInstall}? ${result}"""
@@ -155,4 +156,29 @@ def retrieveInstalledPackages() {
     }
     
     return installedPackages
+}
+
+def String mostRecentVersion(List versions) {
+    def sorted = versions.sort(false) { a, b ->
+        List verA = a.tokenize('.')
+        List verB = b.tokenize('.')
+
+        def commonIndices = Math.min(verA.size(), verB.size())
+
+        for (int i = 0; i < commonIndices; ++i) {
+            def numA = verA[i].toInteger()
+            def numB = verB[i].toInteger()
+            echo("comparing $numA and $numB")
+
+            if (numA != numB) {
+                return numA <=> numB
+            }
+        }
+
+        // If we got this far then all the common indices are identical, so whichever version is longer must be more recent
+        verA.size() <=> verB.size()
+    }
+
+    echo("sorted versions: $sorted")
+    sorted[-1]
 }
