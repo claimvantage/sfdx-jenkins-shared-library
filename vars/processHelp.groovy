d23ab3399f86ca071823e470736ef3891ffc8d55
amcegan <amcegan@users.noreply.github.com>
2022-11-07 11:52:16 +0000

    ABM-1705 Rollback

--- vars/processHelp.groovy
+++ vars/processHelp.groovy
@@ -21,7 +21,7 @@ def call(Map parameters = [:]) {
 
             echo "... extract from Confluence"
 
-            def base64Help = Base64.encoder.encodeToString(exportConfluenceSpaceWithBody(USERPASS, h.rootPageId))
+            def base64Help = Base64.encoder.encodeToString(exportConfluenceSpace(USERPASS, h.rootPageId))
             writeFile file: "exportedHelp.zip", text: base64Help, encoding: "Base64"
         }
 
@@ -117,50 +117,6 @@ def exportConfluenceSpace(String userpass, String rootPageId) {
 
     def connection = new URL(url).openConnection() as HttpURLConnection
     connection.setRequestProperty("Authorization", "Basic ${base64UserColonPassword}")
-
-    return connection.inputStream.bytes
-}
-
-def exportConfluenceSpaceWithBody(String userpass, String rootPageId) {
-    def baseUrl = "${env.CONFLUENCE_BASE_URL}"
-    if (!baseUrl.endsWith("/")) {
-        baseUrl = "${baseUrl}/";
-    }
-    def exportSchemeId = "${env.SCROLL_HTML_EXPORTER_SCHEME_ID}"
-    def url = "${baseUrl}rest/scroll-html/1.0/sync-export?exportSchemeId=${exportSchemeId}&rootPageId=${rootPageId}"
-    def base64UserColonPassword = Base64.encoder.encodeToString(userpass.getBytes())
-
-    def connection = new URL(url).openConnection() as HttpURLConnection
-    connection.setRequestProperty("Authorization", "Basic ${base64UserColonPassword}")
-    connection.setDoOutput(true);
-    connection.setRequestMethod("POST");
-    OutputStream os = connection.getOutputStream();
-    OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
-
-    String body = "{\n" +
-            "    \"exportScheme\": {\n" +
-            "        \"exporter\": {\n" +
-            "            \"properties\": {\n" +
-            "                \"buildSearchIndex\": \"true\",\n" +
-            "                \"exportMode\": \"default\",\n" +
-            "                \"linkNamingStrategy.extendedCharHandling\": \"Ignore\",\n" +
-            "                \"linkNamingStrategy.whitespaceHandling\": \"Blank\",\n" +
-            "                \"linkNamingStrategy.fileNameSchema\": \"PageTitle\",\n" +
-            "                \"linkNamingStrategy.extension\": \"html\"\n" +
-            "            }\n" +
-            "        }\n" +
-            "    },\n" +
-            "    \"rootPageId\": \"136905041\"\n" +
-            "}";
-    echo "*** 0"
-    osw.write(body);
-    echo "*** 1"
-    osw.flush();
-    echo "*** 2"
-    osw.close();
-    echo "*** 3"
-    os.close();  //don't forget to close the OutputStream
-    echo "*** 4"
 
     return connection.inputStream.bytes
 }
